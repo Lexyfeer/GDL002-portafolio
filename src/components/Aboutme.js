39c537ec0e3c581fbd4e013c8a98cb50a423c29d
Nancy <lexyfeer@hotmail.com>
2019-06-27 09:09:29 -0500

    Agregue 2 media queries para moviles mas pequeños

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -10,35 +10,35 @@ class Aboutme extends React.Component {
   //   super();
   // this.state = {name: 'Nancy'};
   // }
-//   render() {
-//     return <div>My Component {this.state.name}</div>;
-//   }
-// };
+  //   render() {
+  //     return <div>My Component {this.state.name}</div>;
+  //   }
+  // };
 
-render() {
-  return (
-    <div className="mainContainer">
-      <div className="contentAboutme">
-        <h2>Algo sobre mi</h2> <br />
-        <div className="containerProfilePicture">
-          <img src={PictureProfile} className="profilePicture" alt="Foto de perfil" />
-        </div>
-        <p>Mi nombre completo es **Nancy María de la Luz Rodríguez López** soy de Guadalajara, Jalisco, antes de adentrarme en el mundo de la tecnologia, estudie Licenciatura en nutrcion y soy pasante de la misma, despues de eso mi campo laboral habia sido hasta ahora como recepcionista, secretaria o en ventas.</p> <br />
-        <p>Agunos de mis pasatiempos son:</p>
-        <div className="containerHobbies">
-          <label>Música</label>
-          <img className="hobbies" src={iconMusic} alt="Escuchar música" />
-          <label>Videojuegos</label>
-          <img className="hobbies" src={iconVideoGames} alt="Videojuegos" />
-          <label>Películas</label>
-          <img className="hobbies" src={iconMovies} alt="Películas" />
-          <label>Pump it up</label>
-          <img className="hobbies" src={iconPumpItUp} alt="Pump it up" />
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className="contentAboutme">
+          <h2>Algo sobre mi</h2> <br />
+          <div className="containerProfilePicture">
+            <img src={PictureProfile} className="profilePicture" alt="Foto de perfil" />
+          </div>
+          <p>Mi nombre completo es **Nancy María de la Luz Rodríguez López** soy de Guadalajara, Jalisco, antes de adentrarme en el mundo de la tecnología, estudié Licenciatura en nutrción y soy pasante de la misma, después de eso mi campo laboral había sido hasta ahora como recepcionista, secretaria o en ventas.</p> <br />
+          <p>Agunos de mis pasatiempos son:</p>
+          <div className="containerHobbies">
+            <label>Música</label>
+            <img className="hobbies" src={iconMusic} alt="Escuchar música" />
+            <label>Videojuegos</label>
+            <img className="hobbies" src={iconVideoGames} alt="Videojuegos" />
+            <label>Películas</label>
+            <img className="hobbies" src={iconMovies} alt="Películas" />
+            <label>Pump it up</label>
+            <img className="hobbies" src={iconPumpItUp} alt="Pump it up" />
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default Aboutme;
