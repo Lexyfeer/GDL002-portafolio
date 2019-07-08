5d6279e252827b82a5485a10e2ddf8a6465058b7
Nancy <lexyfeer@hotmail.com>
2019-07-08 12:38:54 -0500

    Cambie la tipografia beon porque algo paso con esa libreria que ya no la estaba agarrando

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 
-class Contact extends React.Component {
+class Contact extends Component {
   // constructor () {
   //   super()
   // }
 
   render() {
     return (
-      <div className="mainContainer">
+      <Fragment>
         <div className="contentContact">
-          <h2>Hola!, soy un componente de CONTACTAME</h2>
+          <h2>¿Te gustaría que trabajemos juntos?</h2>
+          <p>¡Contáctame ahora mismo!</p>
 
+          <form className="formContact">
+            <div className="form-group">
+              <label>Tu email</label>
+              <input type="email" className="form-control" id="email" placeholder="Escribe tu correo electrónico" />
+            </div>
+            <div className="form-group">
+              <label>Asunto</label>
+              <input className="form-control form-control-sm" id="subject" type="text" placeholder="Asunto" />
+            </div>
+            <div className="form-group">
+              <label>Mensaje</label>
+              <textarea className="form-control" id="message" rows="3"></textarea>
+            </div>
+          </form>
 
+          <a href="https://res.cloudinary.com/lexyfeer/image/upload/v1562606922/CV_Nancy_2.pdf" download="Curriculum CV-Nancy.pdf" rel="noopener noreferrer" target="_blank">
+            DESCARGA MI CV AQUÍ
+          </a>
         </div>
-      </div>
-
+      </Fragment>
     )
   }
 }
