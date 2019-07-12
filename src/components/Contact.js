87ae8e6ff46e2221d31a47d255f69b4f74fdc9a7
Nancy <lexyfeer@hotmail.com>
2019-07-12 15:53:03 -0500

    Ajustando contenedores con vh para que se ajustaran de acuerdo al total de la pantalla segun media querie

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,6 @@ import icoPDF from '../assets/images/icoPdf.png';
 
 
 class Contact extends Component {
-  // constructor () {
-  //   super()
-  // }
-
   render() {
     return (
       <Fragment>
