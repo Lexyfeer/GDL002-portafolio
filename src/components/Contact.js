2ea41371149b1b3c6fe134c4860c629e2d42c5c6
Nancy <lexyfeer@hotmail.com>
2019-08-20 21:19:48 -0500

    Agregue info al apartado de contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,11 +7,24 @@ class Contact extends Component {
     return (
       <Fragment>
         <div className="contentContact">
-          <h2>¿Te gustaría que trabajaramos juntos?</h2>
+          <h2>¿Te gustaría que trabajáramos juntos?</h2>
 
-          <a href="mailto:lexyfeer@hotmail.com?Subject=Vi%20tu%20portafolio%20y%20me%20interesa%20trabajar%20contigo" rel="noopener noreferrer">
+          <p> Tan solo:</p>
+          <ol className="contactMe">
+            <li>Completa los datos del formulario.</li>
+            <li>Da click en ENVIAR.</li>
+            <li>Haz click en la validación de que no eres un robot.</li>
+          </ol>
+
+          <p> ¡Y listo! tratare de ponerme en contacto contigo lo antes posible, ya sea vía correo o e-mail según como tu me indiques en el mensaje.</p>
+
+          <p><strong>Ó descarga mi CV y ponte en contacto conmigo directamente vía telefónica o Linkedin.</strong></p>
+
+          <h3>¡HASTA PRONTO!</h3>
+
+          {/* <a href="mailto:lexyfeer@hotmail.com?Subject=Vi%20tu%20portafolio%20y%20me%20interesa%20trabajar%20contigo" rel="noopener noreferrer">
               ENVÍAME UN CORREO AQUÍ
-          </a>
+          </a> */}
 
           <form className="formContact" action="https://formspree.io/lexyfeer@hotmail.com" method="POST">
           {/* action="mailto:lexyfeer@hotmail.com" method="post" enctype="text/plain" */}
