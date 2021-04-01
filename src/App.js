907abe554b76784532616b6d853cae5013784044
Nancy Ma Rodriguez Lopez <lexyfeer@hotmail.com>
2021-03-31 19:42:42 -0600

    Agregue sass al proyecto y redimensione las imagenes que se utilizan dentro del proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import './App.scss';
+
 import { HashRouter, Route } from 'react-router-dom';
-import './App.css';
-import Navigation from './components/Navigation';
-import Home from './components/Home';
+
 import Aboutme from './components/Aboutme';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/footer';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
+import React from 'react';
+import Skills from './components/Skills';
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, {Component, Fragment} from 'react';
 
-
 class Home extends Component {
   render() {
     return (
@@ -9,6 +8,11 @@ class Home extends Component {
           <div className="post">
             <h2>Nancy Rodríguez López</h2>
             <p>Front-end Developer</p>
+
+            <div className="txtHome">
+              <p>El desarrollo de este proyecto esta inspirado en los años 80s, y el juego de Double Dragon, ¡los colores neon me encantan!.</p>
+              <p>Esta es una Single Page Application, fue desarrollado desde 0 utilizando ReactJS, CSS3 y Bootstrap; cuenta con una interfaz responsiva programada para una adaptación fluida en cualquier tipo de pantalla con Media Queries y Flexbox (Responsive Web Design).</p>
+            </div>
           </div>
         </div>
       </Fragment>
