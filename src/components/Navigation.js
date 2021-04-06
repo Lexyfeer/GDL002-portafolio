d5b7a84fcbfcd89fdbe005aec7734788514d2597
Nancy Ma Rodriguez Lopez <lexyfeer@hotmail.com>
2021-04-05 22:35:21 -0500

    Resposive completo y cambio de imagen del home

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import Logo from '../assets/images/LogoPortfolio.png';
 import React from 'react';
 
 const Navigation = () => (
-    <Navbar variant="dark" expand="md" sticky="top">
+    <Navbar variant="dark" expand="lg" sticky="top">
         <Navbar.Brand>
             <Link to="/">
                 <img className="App-logo-image" src={Logo} alt="logo" />
@@ -15,31 +15,31 @@ const Navigation = () => (
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
                 <Nav.Item>
-                    <NavLink className="navMenuLink" exact to="/">
+                    <NavLink className="-navMenuLink" exact to="/">
                         <h6>Inicio</h6>
                     </NavLink>
                 </Nav.Item>
                 <Nav.Item>
-                    <NavLink className="navMenuLink" to="/Aboutme">
+                    <NavLink className="-navMenuLink" to="/Aboutme">
                         <h6>Acerca de mí</h6>
                     </NavLink>
                 </Nav.Item>
 
                 <Nav.Item>
-                    <NavLink className="navMenuLink" to="/Portfolio">
+                    <NavLink className="-navMenuLink" to="/Portfolio">
                         <h6>Proyectos</h6>
                     </NavLink>
                 </Nav.Item>
 
                 <Nav.Item>
-                    <NavLink className="navMenuLink" to="/Skills">
+                    <NavLink className="-navMenuLink" to="/Skills">
                         <h6>Habilidades</h6>
                     </NavLink>
                 </Nav.Item>
 
                 <Nav.Item>
-                    <NavLink className="navMenuLink" to="/Contact">
-                        <h6>Contacto y CV</h6>
+                    <NavLink className="-navMenuLink" to="/Contact">
+                        <h6>Contacto</h6>
                     </NavLink>
                 </Nav.Item>
             </Nav>
